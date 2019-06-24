0c1883e94763fe80b221f39c6750e35eaf58e2bd
Eacaw <davidpinchen@gmail.com>
2019-06-24 15:49:02 +0100

    Project Complete

--- src/com/davidpinchen/DNA.java
+++ src/com/davidpinchen/DNA.java
@@ -2,18 +2,76 @@ package com.davidpinchen;
 
         import static java.lang.Math.random;
 
-class DNA {
+class DNA implements Comparable<DNA> {
 
-    private char[] genes = new char[40];
+    private char[] genes;
+    public int fitness = 0;
 
-    DNA(){
+    DNA(int targetLength){
+        this.genes = new char[targetLength];
         for (int i = 0; i < genes.length; i++){
             genes[i] = (char) ((random() * 128) + 32);
         }
-        System.out.println(genes);
     }
 
     public char[] getGenes() {
         return genes;
     }
+
+    void evaluateFitness(char[] targetPhrase){
+        this.fitness = 0;
+        for (int i = 0; i < this.getGenes().length; i++){
+            if (this.getGenes()[i] == targetPhrase[i]){
+                this.fitness++;
+            }
+        }
+    }
+
+    public int getFitness() {
+        return fitness;
+    }
+
+    @Override
+    public java.lang.String toString(){
+        StringBuilder builder = new StringBuilder();
+        for (char gene : this.getGenes()){
+            builder.append(gene);
+        }
+        String currentDNA = builder.toString();
+
+        return "" + currentDNA + "\t\t Fitness: " + this.fitness;
+    }
+
+    @Override
+    public int compareTo(DNA o) {
+        return o.getFitness() - this.getFitness();
+    }
+
+    public DNA crossover(DNA parent2){
+        DNA child = new DNA(this.genes.length);
+
+        int midpoint = (int) (random() * genes.length);
+
+        for (int i = 0; i < genes.length; i++){
+            if (i > midpoint){
+                child.genes[i] = this.genes[i];
+            } else {
+                child.genes[i] = parent2.genes[i];
+            }
+        }
+
+        return child;
+    }
+
+    public void mutate(int mutationRate){
+        for (int i = 0; i < genes.length; i++){
+            if (random() < (float) mutationRate / 100){
+                genes[i] = (char) ((random() * 128) + 32);
+            }
+        }
+
+
+    }
+
+
 }
--- src/com/davidpinchen/Main.java
+++ src/com/davidpinchen/Main.java
@@ -1,12 +1,20 @@
 package com.davidpinchen;
 
+        import java.util.ArrayList;
+        import java.util.Arrays;
+
 public class Main {
 
+
+    private static int populationSize = 100;
+    private static DNA[] population = new DNA[populationSize];
     private static String targetString = "To be or not to be, that is the question!";
     private static char[] targetPhrase = targetString.toCharArray();
+    private static int targetLength = targetString.length();
 
-    private static DNA[] population = new DNA[100];
-    private static int[] populationFitness = new int[100];
+    private static ArrayList<DNA> matingPool = new ArrayList<>();
+
+    private static int generation = 0;
 
     public static void main(String[] args) {
         System.out.println("TypewritingMonkeys");
@@ -14,26 +22,62 @@ public class Main {
         System.out.println("Target phrase is:");
         System.out.println(targetString);
         System.out.println("------------------");
-        setup();
+        generateInitialPopulation(targetPhrase);
+
+        boolean complete = false;
+
+        while(!complete) {
+            evaluateFitness(targetPhrase);
+            sortPopulation();
+            System.out.println(population[0].toString());
+            if (population[0].fitness == targetLength) {
+                System.out.println("Completed in " + generation + " generations");
+                complete = true;
+            }
+            fillMatingPool();
+            breedNextGeneration();
+            generation++;
+        }
+        System.out.println("------------------");
 
     }
 
-    private static void setup(){
+    private static void generateInitialPopulation(char[] targetPhrase){
         for (int i = 0; i < population.length; i++){
-            population[i] = new DNA();
+            population[i] = new DNA(targetLength);
         }
     }
 
-    private static void evaluateFitness(DNA populationInstance, int index){
-        int fitness = 0;
-        for (int i = 0; i < populationInstance.getGenes().length; i++){
-            if (populationInstance.getGenes()[i] == targetPhrase[i]){
-                fitness++;
+    private static void evaluateFitness(char[] targetPhrase){
+        for (DNA dna : population) {
+            dna.evaluateFitness(targetPhrase);
+        }
+    }
+
+    private static void sortPopulation(){
+        Arrays.sort(population, DNA::compareTo);
+    }
+
+    private static void fillMatingPool(){
+        matingPool.clear();
+        int oddValueCatch = (population.length / 2) % 2; // Catch uneven numbers to ensure mating pool always has an even population
+        matingPool.addAll(Arrays.asList(population).subList(0, (population.length/2) - oddValueCatch));
+    }
+
+    private static void breedNextGeneration(){
+        DNA[] nextGeneration = new DNA[populationSize];
+        int currentIndex = 0;
+        for (int i = 0; i < (matingPool.size() / 2); i ++){
+            for (int j = 0; j < 4; j++) {
+                DNA child = population[2 * i].crossover(population[(2 * i) + 1]);
+                int mutationRate = 1; // percentage amount for mutations to occur, 1 - 100
+                child.mutate(mutationRate);
+                nextGeneration[currentIndex] = child;
+                currentIndex++;
             }
         }
-        populationFitness[index] = fitness;
+        population = nextGeneration;
     }
-
 
 }
 
